8d614ade5a73af457f4ebfa768d6f2bb560dd6b3
paganini <pcid111@naver.com>
2023-02-06 13:46:00 +0900

    'tlqkf'

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>바이챈스템-관리자</title>
     <!-- css -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('/css/admin.css')}}">
     
     
@@ -26,13 +25,13 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ route('logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">로그아웃 기능</a>
+                        <a href="{{ route('logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">로그아웃</a>
                     @else
                         <!-- <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">로그인</a> -->
 
-                        @if (Route::has('register'))
+                        <!-- @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">회원가입</a>
-                        @endif
+                        @endif -->
                     @endauth
                 </div>
             @endif
